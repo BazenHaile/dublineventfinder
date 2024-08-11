d9bbbe517fbdbf4c69863f331389932588f3d153
BazenHaile <113728471+BazenHaile@users.noreply.github.com>
2024-08-11 10:38:27 +0100

    Update project settings.py file for Render deployment

--- dublineventfinder/settings.py
+++ dublineventfinder/settings.py
@@ -20,8 +20,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['dublineventfinder-spjl.onrender.com', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://dublineventfinder-spjl.onrender.com']
+ALLOWED_HOSTS = ['dublineventfinder.onrender.com', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://dublineventfinder.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
